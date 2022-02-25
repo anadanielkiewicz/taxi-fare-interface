d92561b959c5be7ff59a64733df961b1887ae50e
Anna Jusko <anna.danielkiewicz@gmail.com>
2022-02-25 11:45:49 +0100

    testing 1

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-let taxiFareApiUrl = 'http://localhost:8001/predict'; // replace with your API endpoint
+let taxiFareApiUrl = 'https://taxifare.lewagon.com/predict'; // replace with your API endpoint
 const centralCoordinates = [-74.00597, 40.71427]; // starting position [lng, lat]
 
 if (window.location.href.includes('https://taxifare.lewagon.com')) {
   taxiFareApiUrl = 'https://taxifare.lewagon.ai/predict';
 }
 
-mapboxgl.accessToken = 'pk.eyJ1Ijoia3Jva3JvYiIsImEiOiJja2YzcmcyNDkwNXVpMnRtZGwxb2MzNWtvIn0.69leM_6Roh26Ju7Lqb2pwQ';
+mapboxgl.accessToken = 'pk.eyJ1IjoiYW5uZGFuIiwiYSI6ImNsMDI5ZXp4bTExb3AzanFwZnRjcXl6bHcifQ.6xcSEYhjMj_ogk5jBA4fag';
 
 const displayMap = (start, stop) => {
   const map = new mapboxgl.Map({
